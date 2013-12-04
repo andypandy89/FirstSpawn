23683cd1ca1671057e4f47433107f45b890f63e3
andypandy89 <andypandy89@ultimateminecraft.net>
2013-12-05 00:15:06 +0100

    Implemented Help-menu, Config-reload and some more code cleanup.

--- src/main/java/no/andy/firstspawn/FirstSpawn.java
+++ src/main/java/no/andy/firstspawn/FirstSpawn.java
@@ -26,6 +26,7 @@ public class FirstSpawn extends JavaPlugin implements Listener {
         createPluginFolder();
         getConfig().options().copyDefaults(true);
         saveConfig();
+        this.getDescription().getPermissions();
         getServer().getPluginManager().registerEvents(this, this);
         if ((getConfig().getString("location.world")).equals("NotConfigured")) {
             World defWorld = getServer().getWorlds().get(0);
@@ -52,7 +53,8 @@ public class FirstSpawn extends JavaPlugin implements Listener {
             if (command.startsWith("/spawn")) {
                 event.setMessage("/firstspawn");
                 event.setCancelled(true);
-                event.getPlayer().teleport(new Location(Bukkit.getWorld(getConfig().getString("location.world")), getConfig().getDouble("location.x"), getConfig().getDouble("location.y"), getConfig().getDouble("location.z"), (float) getConfig().getDouble("location.yaw"), (float) getConfig().getDouble("location.pitch")));
+                tpToFirstSpawn(event.getPlayer());
+                return;
             }
         }
     }
@@ -60,16 +62,19 @@ public class FirstSpawn extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("firstspawn")) {
+            String spawnPerm = "firstspawn.spawn";
+            String sendPerm = "firstspawn.send";
+            String setPerm = "firstspawn.set";
+            String reloadPerm = "firstspawn.reload";
             String prefix = ChatColor.GRAY + "[" + ChatColor.GREEN + this.getDescription().getName() + ChatColor.GRAY + "] ";
             if (args.length == 0) {
                 if (sender instanceof Player) {
                     Player player = (Player) sender;
-                    if (!sender.hasPermission("firstspawn.spawn")) {
+                    if (!sender.hasPermission(spawnPerm)) {
                         sender.sendMessage(prefix + ChatColor.RED + "You do not have permission for that command.");
                         return true;
                     } else {
-                        sender.sendMessage(prefix + ChatColor.WHITE + "Teleporting to first spawn...");
-                        player.teleport(new Location(Bukkit.getWorld(getConfig().getString("location.world")), getConfig().getDouble("location.x"), getConfig().getDouble("location.y"), getConfig().getDouble("location.z"), (float) getConfig().getDouble("location.yaw"), (float) getConfig().getDouble("location.pitch")));
+                        tpToFirstSpawn(player);
                         return true;
                     }
                 } else {
@@ -79,7 +84,7 @@ public class FirstSpawn extends JavaPlugin implements Listener {
             } else if (args.length == 1 && args[0].equalsIgnoreCase("set")) {
                 if (sender instanceof Player) {
                     Player player = (Player) sender;
-                    if (sender.hasPermission("firstspawn.set")) {
+                    if (sender.hasPermission(setPerm)) {
                         Location loc = player.getLocation();
                         getConfig().set("location.world", loc.getWorld().getName());
                         getConfig().set("location.x", loc.getX());
@@ -99,13 +104,43 @@ public class FirstSpawn extends JavaPlugin implements Listener {
                 }
             } 
             else if (args.length == 1 && args[0].equalsIgnoreCase("help")) {
-                // TODO: Implement help menu
+                sender.sendMessage(ChatColor.WHITE + "----------------" + ChatColor.GRAY + "[" + ChatColor.GREEN + this.getDescription().getName() + " Help Menu " + ChatColor.GRAY + "]" + ChatColor.WHITE + "----------------");
+                if (sender instanceof Player) {
+                    if (sender.hasPermission(spawnPerm)) {
+                        sender.sendMessage(ChatColor.GREEN + "/firstspawn" + ChatColor.WHITE + " - " + ChatColor.GRAY + "Teleports you to first spawn.");
+                    }
+                }
+                if(sender.hasPermission(sendPerm)) {
+                    sender.sendMessage(ChatColor.GREEN + "/firstspawn <player>" + ChatColor.WHITE + " - " + ChatColor.GRAY + "Teleports specified player to first spawn.");
+                }
+                if(sender instanceof Player && sender.hasPermission(setPerm)) {
+                    sender.sendMessage(ChatColor.GREEN + "/firstspawn set" + ChatColor.WHITE + " - " + ChatColor.GRAY + "Sets the first spawn at current location.");
+                }
+                if(sender.hasPermission(reloadPerm)) {
+                    sender.sendMessage(ChatColor.GREEN + "/firstspawn reload" + ChatColor.WHITE + " - " + ChatColor.GRAY + "Reloads the configuration file.");
+                }
+                return true;
+            }
+            else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission(reloadPerm)) {
+                    sender.sendMessage(prefix + ChatColor.RED + "You do not have permission for that command.");
+                    return true;
+                }
+                else {
+                    this.reloadConfig();
+                    sender.sendMessage(prefix + ChatColor.WHITE + "Configuration file reloaded.");
+                    return true;
+                }
             }
             else if (args.length == 1) {
                 if (sender instanceof Player) {
-                    if (!sender.hasPermission("firstspawn.send")) {
+                    if (!sender.hasPermission(sendPerm)) {
                         sender.sendMessage(prefix + ChatColor.RED + "You do not have permission for that command.");
                         return true;
+                    }
+                    else {
+                        sender.sendMessage(prefix + ChatColor.RED + "You do not have permission for that command.");
+                        return true; 
                     }
                 }
                 Player player = getServer().getPlayerExact(args[0]);
@@ -113,8 +148,7 @@ public class FirstSpawn extends JavaPlugin implements Listener {
                     sender.sendMessage(prefix + ChatColor.RED + "Player " + args[0] + " not found.");
                     return true;
                 } else {
-                    player.sendMessage(prefix + ChatColor.WHITE + "Teleporting to first spawn...");
-                    player.teleport(new Location(Bukkit.getWorld(getConfig().getString("location.world")), getConfig().getDouble("location.x"), getConfig().getDouble("location.y"), getConfig().getDouble("location.z"), (float) getConfig().getDouble("location.yaw"), (float) getConfig().getDouble("location.pitch")));
+                    tpToFirstSpawn(player);
                     sender.sendMessage(prefix + ChatColor.WHITE + "Teleported " + args[0] +" to first spawn.");
                     return true;
                 }
@@ -135,12 +169,23 @@ public class FirstSpawn extends JavaPlugin implements Listener {
         if (!player.hasPlayedBefore() && !getConfig().getString("location.world").equals("NotConfigured")) {
             // Reason for waiting two ticks before execution is to make sure other plugins don't get in the way.
             getServer().getScheduler().runTaskLater(this, new Runnable() {
-
                 public void run() {
-                    player.teleport(new Location(Bukkit.getWorld(getConfig().getString("location.world")), getConfig().getDouble("location.x"), getConfig().getDouble("location.y"), getConfig().getDouble("location.z"), (float) getConfig().getDouble("location.yaw"), (float) getConfig().getDouble("location.pitch")));
+                    tpToFirstSpawn(player);
                 }
             }, 2L);
         }
-
+    }
+    
+    public void tpToFirstSpawn(Player p) {
+        String prefix = ChatColor.GRAY + "[" + ChatColor.GREEN + this.getDescription().getName() + ChatColor.GRAY + "] ";
+        World w = Bukkit.getWorld(getConfig().getString("location.world"));
+        double x = getConfig().getDouble("location.x");
+        double y = getConfig().getDouble("location.y");
+        double z = getConfig().getDouble("location.z");
+        float yaw = (float) getConfig().getDouble("location.yaw");
+        float pitch = (float) getConfig().getDouble("location.pitch");
+        Location dest = new Location(w, x, y, z, yaw, pitch);
+        p.sendMessage(prefix + ChatColor.WHITE + "Teleporting to first spawn...");
+        p.teleport(dest);
     }
 }
